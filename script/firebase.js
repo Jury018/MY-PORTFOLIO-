111a9f65c460b64fb2424464b4ee96404e927d10
BONFIRE <165536559+Jury018@users.noreply.github.com>
2025-03-21 20:01:51 +0800

    Update firebase.js
    Signed-off-by: BONFIRE  <165536559+Jury018@users.noreply.github.com>

--- script/firebase.js
+++ script/firebase.js
@@ -3,20 +3,19 @@ import { getFirestore, collection, addDoc, doc, getDoc, updateDoc, setDoc } from
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBJvk0qhwZmLCpwKOhn1lWw1F3DHDucqF8",
-  authDomain: "new-portfolio-9c126.firebaseapp.com",
-  databaseURL: "https://new-portfolio-9c126-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "new-portfolio-9c126",
-  storageBucket: "new-portfolio-9c126.firebasestorage.app",
-  messagingSenderId: "213335134410",
-  appId: "1:213335134410:web:3704a6aeb8634781962433",
-  measurementId: "G-4DF86P5TYK"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 
 const viewDocRef = doc(db, "view", "viewCount");
 
